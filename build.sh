d4813c90ceb89874aa835c7443cf9e57354124e2
Chemaclass <chemaclass@outlook.es>
2024-09-18 09:53:04 +0200

    feat: improve build.sh adding optional semantic versioning

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ function generate_bin() {
     } >> "$temp"
   done
 
-  cat entry-point >> "$temp"
+  cat "$ENTRY_POINT" >> "$temp"
   grep -v '^source' "$temp" > "$out"
   rm "$temp"
   chmod u+x "$out"
@@ -49,10 +49,79 @@ function generate_checksum() {
   echo "$checksum"
 }
 
+function get_current_version() {
+  local file_with_version=$1
+  grep "declare -r $VERSION_VAR_NAME=" "$file_with_version" \
+      | sed "s/declare -r $VERSION_VAR_NAME=\"\([0-9]*\.[0-9]*\.[0-9]*\)\"/\1/"
+}
+
+function update_version() {
+  local version_type=$1
+  local file_with_version=$2
+
+  local current_version
+  current_version=$(grep "declare -r $VERSION_VAR_NAME=" "$file_with_version" \
+    | sed "s/declare -r $VERSION_VAR_NAME=\"\([0-9]*\.[0-9]*\.[0-9]*\)\"/\1/")
+
+  IFS='.' read -r major minor patch <<< "$current_version"
+
+  # Increment the appropriate version part
+  case "$version_type" in
+    major)
+      major=$((major + 1))
+      ;;
+    minor)
+      minor=$((minor + 1))
+      ;;
+    patch)
+      patch=$((patch + 1))
+      ;;
+    *)
+      echo "Invalid version increment option: $version_type. Use major, minor, or patch."
+      exit 1
+      ;;
+  esac
+
+  local new_version="$major.$minor.$patch"
+  local search_pattern="declare -r $VERSION_VAR_NAME=\"[0-9]*\.[0-9]*\.[0-9]*\""
+  local replace_pattern="declare -r $VERSION_VAR_NAME=\"$new_version\""
+
+  sed -i.bak \
+    "s/$search_pattern/$replace_pattern/" \
+    "$file_with_version"
+
+  rm "${file_with_version}.bak"
+
+  echo "$new_version"
+}
+
 ########################
 #         MAIN         #
 ########################
-OUT_DIR=${1:-"bin"}
+VERSION_VAR_NAME="BASH_SKELETON_VERSION"
+ENTRY_POINT="entry-point"
+OUT_DIR="bin"
+NEW_VERSION_TYPE=""
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --new)
+      shift
+      NEW_VERSION_TYPE="$1"
+      ;;
+    *)
+      OUT_DIR="$1"
+      ;;
+  esac
+  shift
+done
+
+if [[ "$NEW_VERSION_TYPE" != "" ]]; then
+  echo "Updated version: $(update_version "$NEW_VERSION_TYPE" "$ENTRY_POINT")"
+else
+  echo "Current version: $(get_current_version "$ENTRY_POINT")"
+fi
 
 mkdir -p "$OUT_DIR"
 build "$OUT_DIR/entry-point"
