f5af86e42af9355ae9e13e595eb915d16fd3495d
Chemaclass <chemaclass@outlook.es>
2024-10-05 18:40:08 +0200

    deps: install bashunit:beta, create-pr:0.6, dumper:0.1

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-## bashunit
-curl -s https://bashunit.typeddevs.com/install.sh | bash -s lib 0.16.0
+# bashunit
+curl -s https://bashunit.typeddevs.com/install.sh | bash -s lib beta
+
+function download() {
+  local url=$1
+  local destination=$2
+  local name=${3:-$(basename "$destination")}
+
+  if curl -s -L "$url" -o "$destination"; then
+    echo "> $name installed successfully in '$(dirname "$destination")'"
+    chmod +x "$destination"
+  else
+    echo "Failed to install $name!"
+    exit 1
+  fi
+}
+
+# create-pr
+download "https://github.com/Chemaclass/create-pr/releases/download/0.6/create-pr" \
+  "lib/create-pr"
+
+# bash-dumper
+download "https://github.com/Chemaclass/bash-dumper/releases/download/0.1/dumper.sh" \
+  "src/dev/dumper.sh"
--- tests/unit/helpers_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function test_helper_current_dir() {
-  assert_same "tests/unit" "$(current_dir)"
-}
-
-function test_helper_current_filename() {
-  assert_same "helpers_test.sh" "$(current_filename)"
-}
